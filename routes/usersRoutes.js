abeb4d0e3b07a386ae3c4524c37aab9f7bb6c882
MPulgarA <matiaspulgaralarcon@gmail.com>
2021-10-29 11:57:07 -0300

    Cambio llamado funcion createWithImage por create user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,7 +5,9 @@ module.exports = (app, upload) => {
     app.get('/api/users/getAll', UsersController.getAll);
     app.get('/api/users/findById/:id', passport.authenticate('jwt', { session: false }), UsersController.findById);
 
-    app.post('/api/users/create', upload.array('image', 1), UsersController.registerWithImage);
+    // este es el bueno
+    // app.post('/api/users/create', upload.array('image', 1), UsersController.registerWithImage);
+    app.post('/api/users/create', UsersController.register);
     app.post('/api/users/login', UsersController.login);
     app.post('/api/users/login', UsersController.logout);
 
